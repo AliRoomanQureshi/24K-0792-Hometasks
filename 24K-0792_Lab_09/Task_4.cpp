99a8281696d2314b3abfb9d834eaf0db60ab59ba
Ali Rooman <aliroomanq@gmail.com>
2025-11-14 10:09:27 -0600

    Create Task_4.cpp

---
+++ 24K-0792_Lab_09/Task_4.cpp
@@ -0,0 +1,112 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    int key;
+    Node* left;
+    Node* right;
+    int height;
+
+    Node(int val) {
+        key = val;
+        left = right = NULL;
+        height = 1;
+    }
+};
+
+int getHeight(Node* n) {
+    if (n == NULL) return 0;
+    return n->height;
+}
+
+int getBalance(Node* n) {
+    if (n == NULL) return 0;
+    return getHeight(n->left)-getHeight(n->right);
+}
+
+Node* rightRotate(Node* y) {
+    Node* x = y->left;
+    Node* T2 = x->right;
+
+    x->right = y;
+    y->left = T2;
+
+    y->height = 1 + max(getHeight(y->left), getHeight(y->right));
+    x->height = 1 + max(getHeight(x->left), getHeight(x->right));
+
+    return x; 
+}
+
+Node* leftRotate(Node* x) {
+    Node* y = x->right;
+    Node* T2 = y->left;
+
+    y->left = x;
+    x->right = T2;
+
+    x->height = 1 + max(getHeight(x->left), getHeight(x->right));
+    y->height = 1 + max(getHeight(y->left), getHeight(y->right));
+
+    return y;
+}
+
+Node* insertNode(Node* node, int key) {
+
+    if (node == NULL)
+        return new Node(key);
+
+    if (key < node->key)
+        node->left = insertNode(node->left, key);
+
+    else if (key > node->key)
+        node->right = insertNode(node->right, key);
+
+    else
+        return node;
+
+    node->height = 1 + max(getHeight(node->left), getHeight(node->right));
+
+    int balance = getBalance(node);
+
+    if (balance > 1 && key < node->left->key)
+        return rightRotate(node);
+
+    if (balance < -1 && key > node->right->key)
+        return leftRotate(node);
+
+    if (balance > 1 && key > node->left->key) {
+        node->left = leftRotate(node->left);
+        return rightRotate(node);
+    }
+
+    if (balance < -1 && key < node->right->key) {
+        node->right = rightRotate(node->right);
+        return leftRotate(node);
+    }
+
+    return node;
+}
+
+void inorder(Node* root) {
+    if (root == NULL) return;
+    inorder(root->left);
+    cout<<root->key<<" ";
+    inorder(root->right);
+}
+
+int main() {
+
+    Node* root = NULL;
+
+    int arr[] = {15, 10, 20, 8, 12, 25, 30};
+    for (int x : arr)
+        root = insertNode(root, x);
+
+    root = insertNode(root, 16);
+
+    cout<<"Inorder after insertion: ";
+    inorder(root);
+
+    return 0;
+}
